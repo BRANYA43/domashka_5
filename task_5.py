bf7a286a419781766ec6d3233dee4b9722acd2d9
Timchenko <tbd2504@gmail.com>
2022-06-17 21:38:07 +0300

    add new file

---
+++ task_5.py
@@ -0,0 +1,5 @@
+"""
+Написати функцію яка прибере з dict елементи із значеннями None:
+{'first_color': 'Red', 'second_color': 'Green', 'third_color': None}
+Повинно повернути {'first_color': 'Red', 'second_color': 'Green'} # * dict може бути довільним
+"""
