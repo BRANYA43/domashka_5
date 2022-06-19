7cc4a15537c2c2650ab0aa99522bbf191c4e7c4a
Timchenko <tbd2504@gmail.com>
2022-06-19 12:01:33 +0300

    second commit

--- task_3.py
+++ task_3.py
@@ -2,46 +2,38 @@
 Написати функцію яка поверне площу фігури: за замовчуванням трикутника, опціонально.
 На вході 2 величини та параметр типу фігури.
 """
+from random import randint
 
 
 def get_area_figure(n1: int, n2=0, n3=0, figure=0) -> int | float:
-    figures = {
-        'triangle': 0,
-        'rhombus': 1,
-        'polygon': 2,
-        'squad': 3,
-        'rectangle': 4,
-        'parallelogram': 5,
-        'trapezoid': 6,
-        'circle': 7
-    }
-    if figures['triangle'] <= figure <= figures['polygon']:
-        if n2 > 0:
-            return n1 * n2 / 2
+    if figure == 0:
+        return n1 * n2 / 2
 
-    elif figures['squad'] <= figure <= figures['parallelogram']:
-        if n2 > 0:
-            return n1 * n2
-        else:
-            return n1 ** 2
+    elif figure == 1:
+        return n1 * n2
 
-    elif figure == figures['trapezoid']:
-        if n2 > 0 and n3 > 0:
-            return (n1 + n2) * n3 / 2
+    elif figure == 2:
+        return n1 ** 2
 
-    elif figure == figures['circle']:
+    elif figure == 3:
+        return (n1 + n2) * n3 / 2
+
+    elif figure == 4:
         return n1 ** 2 * 3.14
+
+    elif figure == 5:
+        return n1 * n2 * n3 / 2
 
 
 def main():
-    print(get_area_figure(10, 12))
-    print(get_area_figure(10, 12, figure=1))
-    print(get_area_figure(10, 12, figure=2))
-    print(get_area_figure(10, figure=3))
-    print(get_area_figure(10, 12, figure=4))
-    print(get_area_figure(10, 12, figure=5))
-    print(get_area_figure(10, 12, 8, figure=6))
-    print(get_area_figure(10, figure=7))
+    print(f'Трикутник S = {get_area_figure(randint(2, 25), randint(2, 25))}')
+    print(f'Ромб S = {get_area_figure(randint(2, 25), randint(2, 25))}')
+    print(f'Прямокутник S = {get_area_figure(randint(2, 25), randint(2, 25), figure=1)}')
+    print(f'Параллелограмм S = {get_area_figure(randint(2, 25), randint(2, 25), figure=1)}')
+    print(f'Квадрат S = {get_area_figure(randint(2, 25), figure=2)}')
+    print(f'Трапеція S = {get_area_figure(randint(2, 25), randint(2, 25), randint(2, 25), figure=3)}')
+    print(f'Коло S = {get_area_figure(randint(2, 25), figure=4)}')
+    print(f'n-угольник S = {get_area_figure(randint(2, 25), randint(2, 25), randint(2, 25), figure=5)}')
 
 
 if __name__ == '__main__':
