626ded6cb7c62ea8e60337fbdb85e1eeb8c609e5
Timchenko <tbd2504@gmail.com>
2022-06-21 19:52:19 +0300

    third commit

--- task_3.py
+++ task_3.py
@@ -4,8 +4,55 @@
 """
 from random import randint
 
+from task_5 import get_input_number
 
-def get_area_figure(n1: int, n2=0, n3=0, figure=0) -> int | float:
+
+def get_area_figures(figure: int) -> int | float:
+    ret = 0
+    if figure == 0:
+        ret = get_formulas_area_figures(
+            get_input_number('a = '),
+            get_input_number('h = ')
+        )
+    elif figure == 1:
+        ret = get_formulas_area_figures(
+            get_input_number('d1 = '),
+            get_input_number('d2 = ')
+        )
+    elif figure == 2:
+        ret = get_formulas_area_figures(
+            get_input_number('a = '),
+            get_input_number('b = ')
+        )
+    elif figure == 3:
+        ret = get_formulas_area_figures(
+            get_input_number('a = '),
+            get_input_number('h = ')
+        )
+    elif figure == 4:
+        ret = get_formulas_area_figures(
+            get_input_number('a = ')
+        )
+    elif figure == 5:
+        ret = get_formulas_area_figures(
+            get_input_number('a = '),
+            get_input_number('b = '),
+            get_input_number('h = ')
+        )
+    elif figure == 6:
+        ret = get_formulas_area_figures(
+            get_input_number('r = ')
+        )
+    elif figure == 7:
+        ret = get_formulas_area_figures(
+            get_input_number('n = '),
+            get_input_number('a = '),
+            get_input_number('r = ')
+        )
+    return ret
+
+
+def get_formulas_area_figures(n1: int, n2=0, n3=0, figure=0) -> int | float:
     if figure == 0:
         return n1 * n2 / 2
 
@@ -26,14 +73,23 @@ def get_area_figure(n1: int, n2=0, n3=0, figure=0) -> int | float:
 
 
 def main():
-    print(f'Трикутник S = {get_area_figure(randint(2, 25), randint(2, 25))}')
-    print(f'Ромб S = {get_area_figure(randint(2, 25), randint(2, 25))}')
-    print(f'Прямокутник S = {get_area_figure(randint(2, 25), randint(2, 25), figure=1)}')
-    print(f'Параллелограмм S = {get_area_figure(randint(2, 25), randint(2, 25), figure=1)}')
-    print(f'Квадрат S = {get_area_figure(randint(2, 25), figure=2)}')
-    print(f'Трапеція S = {get_area_figure(randint(2, 25), randint(2, 25), randint(2, 25), figure=3)}')
-    print(f'Коло S = {get_area_figure(randint(2, 25), figure=4)}')
-    print(f'n-угольник S = {get_area_figure(randint(2, 25), randint(2, 25), randint(2, 25), figure=5)}')
+    figures = ['Трикутник',
+               'Ромб',
+               'Прямокутник',
+               'Параллелограмм',
+               'Квадрат',
+               'Трапеція',
+               'Коло',
+               'n-кутник'
+               ]
+    for i, elem in enumerate(figures):
+        print(f'{i}. {elem}')
+
+    choose_figure = -1
+    while choose_figure > 7 or choose_figure < 0:
+        choose_figure = get_input_number('Оберіть фігур (від 0-7): ')
+
+    print(get_area_figures(choose_figure))
 
 
 if __name__ == '__main__':
